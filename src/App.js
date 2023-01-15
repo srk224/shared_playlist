0da4153f9534563d7cf948eeeb58a45edb4b8d47
unknown <rocklion224@gmail.com>
2023-01-15 00:33:03 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Make the music room a shared responsibility</h1>
+      <p className="music">"Music every day every time.”</p>
+      <button className='shared_playlist'>Create a shared playlist</button>
+      <h2>How does it work?</h2>
+    </div>
+  );
+}
+
+export default App;
